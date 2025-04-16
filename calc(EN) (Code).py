2cbf420459d5daefe8beab13490fe2b296b13a82
ilya346565 <grmshanim@gmail.com>
2025-04-16 09:30:29 +0500

    Add files via upload

---
+++ calc(EN) (Code).py
@@ -0,0 +1,28 @@
+f_num = float(input("Enter the first number»"))
+oper = input("Enter the operation»")
+s_num = float(input("Enter the second number»"))
+if oper == '+':
+    print(f_num + s_num)
+elif oper == '-':
+    print(f_num - s_num)
+elif oper == '*':
+    print(f_num * s_num)
+elif oper == '/':
+    print(f_num / s_num) 
+else:
+    print("Error")
+prodolzhit = input("Enter 'y' to continue")
+while prodolzhit == 'y':
+    f_num = float(input("Enter the first number»"))
+oper = input("Enter the operation»")
+s_num = float(input("Enter the second number»"))
+if oper == '+':
+    print(f_num + s_num)
+elif oper == '-':
+    print(f_num - s_num)
+elif oper == '*':
+    print(f_num * s_num)
+elif oper == '/':
+    print(f_num / s_num) 
+else:
+    print("Error")
---
+++ calc(RU) (Code).py
@@ -0,0 +1,28 @@
+f_num = float(input("Введите первое число»"))
+oper = input("Введите операцию»")
+s_num = float(input("Введите второе число»"))
+if oper == '+':
+    print(f_num + s_num)
+elif oper == '-':
+    print(f_num - s_num)
+elif oper == '*':
+    print(f_num * s_num)
+elif oper == '/':
+    print(f_num / s_num) 
+else:
+    print("Error")
+prodolzhit = input("Введите 'y' что бы продолжить")
+while prodolzhit == 'y':
+    f_num = float(input("Введите первое число»"))
+oper = input("Введите операцию»")
+s_num = float(input("Введите второе число»"))
+if oper == '+':
+    print(f_num + s_num)
+elif oper == '-':
+    print(f_num - s_num)
+elif oper == '*':
+    print(f_num * s_num)
+elif oper == '/':
+    print(f_num / s_num) 
+else:
+    print("Error")
